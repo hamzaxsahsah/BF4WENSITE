4ff84a2e6006d5c200c74f34e40a3eeb5db90d40
hamzaxsahsah <nanobig2000@gmail.com>
2023-08-06 01:39:28 +0100

    adding service

--- b4aservice/b4aservice_api/admin.py
+++ b4aservice/b4aservice_api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin  
 from .models import Article  
 from .models import Service
+from .models import Member
 # Register your models here.  
 admin.site.register(Article)  
 admin.site.register(Service)  
+admin.site.register(Member) 
--- b4aservice/b4aservice_api/urls.py
+++ b4aservice/b4aservice_api/urls.py
@@ -1,9 +1,12 @@
 from .views import ArticleView
 from .views import ServiceView
+from .views import MemberView
 # Update the import
 from django.urls import path
 
 urlpatterns = [
     path('articles/', ArticleView.as_view()) ,
-     path('services/', ServiceView.as_view()) # Update the URL pattern
+    path('services/', ServiceView.as_view()),
+    path('members/', MemberView.as_view())
+ # Update the URL pattern
 ]
